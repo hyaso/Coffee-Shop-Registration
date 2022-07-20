a7318eca4ed3d2e7e65f4094e5db616ad2b1dea6
hyaso <hyassine21@gmail.com>
2022-07-20 13:14:30 -0400

    Coffee Shop Registration Lab

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using CoffeeShopRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShopRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateUserSubmit(string firstName, string lastName, string email, string password)
+        {
+            CoffeeUser newUser = new CoffeeUser();
+            newUser.FirstName = firstName;
+            newUser.LastName = lastName;
+            newUser.Email = email;
+            newUser.Password = password;
+
+            return RedirectToAction("viewuser", newUser);
+        }
+
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+        public IActionResult ViewUser(CoffeeUser userInfo)
+        {
+            return View(userInfo);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
